69a09c1ab854626c616f86112e6e397fb92d0bcd
Rohit Dalal <dalalrohit102@gmail.com>
2020-04-13 23:28:23 +0530

    Cleaning

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,6 @@ import Contact from './../components/Contact';
 import Footer from './../components/UI/footer';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-AOS.init();
 
 
 
